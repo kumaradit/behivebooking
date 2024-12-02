6879f4a57869dd18757f54d7b4b6432dac1e8efe
kumaradit <kr.aditya.msr@gmail.com>
2024-12-02 20:22:00 +0530

    style changes

--- src/header.tsx
+++ src/header.tsx
@@ -5,10 +5,11 @@ const Header: React.FC = () => {
     <div className="container">
       <div className="header_display">
         <div className="logo">
-          <img src="/image/bhivelogo.png" width="124" height="40" alt="logo"></img>
+        <img className="choose_img" src="/image/gym.png"></img>
+          {/* <img src="/image/bhivelogo.png" width="124" height="40" alt="logo"></img> */}
         </div>
         <div className="call_info">
-          <img src="/image/call.png" width="24" height="24" alt="call"></img>
+          <img src="/image/call.png"  style= {{width:"24",height:"24"}} alt="call"></img>
         </div>
       </div>
     </div>
